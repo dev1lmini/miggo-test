3fa20bd8a25838b1b14e01c9cff5fff0dfcac299
Markiian Linynskyy <devilmini.ru@gmail.com>
2024-12-20 15:18:54 +0200

    feat(track): show velocity and period of ISS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,53 @@ export default function Home() {
   return (
     <main className="w-full h-full">
       <Suspense fallback="Loading...">
-        <Map position={data?.iss_position}>
-          <section className="flex flex-col gap-4">
+        <Map position={data?.position}>
+          <section className="flex flex-col gap-2">
+            {data?.position && (
+              <section className="bg-black/10 px-2 py-1 rounded-md">
+                <section>
+                  <span className="font-medium text-black/60 text-md">
+                    Latitude:{" "}
+                  </span>
+                  <span className="font-semibold text-black/80 text-md">
+                    {data.position.latitude}
+                  </span>
+                </section>
+                <section>
+                  <span className="font-medium text-black/60 text-md">
+                    Longitude:{" "}
+                  </span>
+                  <span className="font-semibold text-black/80 text-md">
+                    {data.position.longitude}
+                  </span>
+                </section>
+                <section>
+                  <span className="font-medium text-black/60 text-md">
+                    Velocity:{" "}
+                  </span>
+                  <span className="font-semibold text-black/80 text-md">
+                    {data.position.velocity} km/h
+                  </span>
+                </section>
+                <section>
+                  <span className="font-medium text-black/60 text-md">
+                    Velocity:{" "}
+                  </span>
+                  <span className="font-semibold text-black/80 text-md">
+                    92.88 min
+                  </span>
+                </section>
+              </section>
+            )}
             {data?.timestamp && (
-              <section>
+              <section className="bg-black/10 px-2 py-1 rounded-md">
                 <p className="font-medium text-black/60 text-md">Updated at</p>
                 <p className="font-semibold text-black/80 text-md">
                   {format(new Date(data?.timestamp * 1000), "PPPpp")}
                 </p>
               </section>
             )}
+
             {error && (
               <p className="font-semibold text-red-700">{error?.message}</p>
             )}
